f7ea467b56017d39fec1c0fe2a8424fcfe06d072
Kaitlyn Ip <kaitlyn.ip@mths.ca>
2022-10-26 00:39:12 +0000

    fixed code

--- numberGuessing.cpp
+++ numberGuessing.cpp
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <random>
+#include <string>
 
 int main() {
     // this function generates a random number
@@ -15,31 +16,33 @@ int main() {
 
     std::random_device rseed;
     std::mt19937 rgen(rseed());                        // mersenne_twister
-    std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
+    std::uniform_int_distribution<int> idist(1, 9);  // [,9]
     randomVariable = idist(rgen);
 
     // input
     std::cout << "This program is a guessing game." << std::endl;
-    std::cout << "Guess a number between 0-9: ";
-    std::cin >> userGuess;
+    std::cout << "Guess a number between 1-9: ";
+    std::cin >> integerAsString;
 
+    userGuess = atoi(integerAsString.c_str());
     // process
 
     try {
         userGuess = std::stoi(integerAsString);
-        std::cout << "That is an integer. " << userGuess;
+        std::cout << "That is an integer. ";
     } catch (std::invalid_argument) {
-        std::cout << "That was not a valid integer";
-
+        std::cout << "That was not a valid integer. ";
+    }
         if (userGuess == randomVariable) {
             // output
-            std::cout << "You guessed the correct number!";
+            std::cout << "You guessed the correct number! ";
         } else {
             // output
-            std::cout << "You guessed the wrong number!";
-            std::cout << "The correct number was " << randomVariable << ".";
+            std::cout << "You guessed the wrong number! ";
+            std::cout << "The correct number was " << randomVariable << ". ";
         }
 
+        std::cout << "Thanks for playing!";
         std::cout << std::endl;
         std::cout << "\nDone." << std::endl;
-    }
+}
